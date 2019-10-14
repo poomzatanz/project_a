258815cd48b56b1fccab8984c40f05efbbc5e728
poomzatan <poomzatan@gmail.com>
2019-10-14 16:33:09 +0700

    t

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(0);
-define('DB_NAME', 'hook');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
+define('DB_NAME', 'heroku_bf071c432fea31f');
+define('DB_USER', 'b2dd41bd5b292d');
+define('DB_PASSWORD', '87ed0380');
+define('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net');
  
 // Create connection
 $db     =   new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
